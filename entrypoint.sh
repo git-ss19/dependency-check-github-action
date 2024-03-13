b53c91c4e57ebb0527eb5bdcaa9ce7c380debe0c
siritas s <siritas@gmail.com>
2024-03-13 16:52:51 +0700

    🐛 : allow other enter and remove path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ all_args=$@
 
 /usr/share/dependency-check/bin/dependency-check.sh ${all_args}
 #
-chmod -Rf 666 $GITHUB_WORKSPACE/reports
+chmod -Rf 755 $GITHUB_WORKSPACE/reports
+chmod -Rf 655 $GITHUB_WORKSPACE/reports/dependency-check-report.*
